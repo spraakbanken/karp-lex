995d49c9c11f0cf14f1db32c4fc7a42db8b93ec3
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-10-06 13:00:16 +0200

    build: add pylintrc

--- karp_lex/__init__.py
+++ karp_lex/__init__.py
@@ -1,1 +1,7 @@
 __version__ = "0.1.0"
+
+
+from karp_lex.application.dtos import EntryDto, EntryDtoDict
+
+
+__all__ = ["EntryDto", "EntryDtoDict"]
---
+++ karp_lex/application/dtos/__init__.py
@@ -0,0 +1,6 @@
+from karp_lex.application.dtos.entry_dto import EntryDto
+
+
+EntryDtoDict = EntryDto[dict]
+
+__all__ = ["EntryDto", "EntryDtoDict"]
